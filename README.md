e7089abfecb154b28aede22d779dfc084ac68b38
Moshiur <howladermoshiur@gmail.com>
2020-03-09 23:17:43 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# REST_API_in_Django
+ Learning how to implement REST API in Django Projects
---
+++ full_project/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/namehere/Documents/Github/REST_API_in_Django/full_project/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
